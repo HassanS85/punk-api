4f25ea11484e42f7abbe0036f22e328194078646
Hassan <hassan.seedat07@gmail.com>
2022-06-13 17:00:43 +0100

    added in new classnames

--- src/component/Filter/cards/Cards.jsx
+++ src/component/Filter/cards/Cards.jsx
@@ -4,8 +4,20 @@ import "./Cards.scss";
 
 const Cards = ({ arr }) => {
     console.log(arr)
+
+    const displayCards = arr.map(beer => {
+        <div className='card'>
+            <h2 className="card_name">{beer.name}</h2>
+            <p className='card__ABV'>{beer.abv}</p>
+            <p className='card__Brewed'>{beer.first_brewed}</p>
+            <p className='card__PH'>{beer.ph}</p>
+            <p className='card__description'>{beer.description}</p>
+        </div>
+    })
+
+
   return (
-    <div>Cards</div>
+    <div>{displayCards}</div>
   )
 }
 
